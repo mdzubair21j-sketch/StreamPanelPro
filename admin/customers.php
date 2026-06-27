304bf8d48a53e7d8793ff4b3e13198429c5f1b8e
mdzubair21j-sketch <md.zubair21j@gmail.com>
2026-06-27 16:47:40 +0600

    Update customers.php

---
+++ admin/customers.php
@@ -0,0 +1,101 @@
+<?php
+
+session_start();
+
+require '../includes/database.php';
+
+$stmt = $pdo->query(
+"SELECT * FROM customers ORDER BY id DESC"
+);
+
+$customers = $stmt->fetchAll();
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>
+
+Customers
+
+</title>
+
+</head>
+
+<body>
+
+<h1>
+
+Customers
+
+</h1>
+
+<a href="customer_add.php">
+
+Add Customer
+
+</a>
+
+<table border="1">
+
+<tr>
+
+<th>ID</th>
+
+<th>Name</th>
+
+<th>Email</th>
+
+<th>Status</th>
+
+<th>Expiry</th>
+
+<th>Action</th>
+
+</tr>
+
+<?php foreach($customers as $c): ?>
+
+<tr>
+
+<td><?= $c['id']; ?></td>
+
+<td><?= $c['fullname']; ?></td>
+
+<td><?= $c['email']; ?></td>
+
+<td><?= $c['status']; ?></td>
+
+<td><?= $c['expiry_date']; ?></td>
+
+<td>
+
+<a href="customer_edit.php?id=<?=$c['id']?>">
+
+Edit
+
+</a>
+
+|
+
+<a href="customer_delete.php?id=<?=$c['id']?>">
+
+Delete
+
+</a>
+
+</td>
+
+</tr>
+
+<?php endforeach; ?>
+
+</table>
+
+</body>
+
+</html>
