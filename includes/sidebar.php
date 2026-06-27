556b876b8088d07aa8361abcbe8bf751c290cee3
mdzubair21j-sketch <md.zubair21j@gmail.com>
2026-06-27 16:23:17 +0600

    Create sidebar.php

---
+++ includes/sidebar.php
@@ -0,0 +1,41 @@
+<div>
+
+<a href="dashboard.php">
+
+Dashboard
+
+</a>
+
+<br>
+
+<a href="customers.php">
+
+Customers
+
+</a>
+
+<br>
+
+<a href="resellers.php">
+
+Resellers
+
+</a>
+
+<br>
+
+<a href="packages.php">
+
+Packages
+
+</a>
+
+<br>
+
+<a href="settings.php">
+
+Settings
+
+</a>
+
+</div>
