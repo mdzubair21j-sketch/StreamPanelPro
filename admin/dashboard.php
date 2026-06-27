13c6c398a9378a8b6ab6b5dd6e3d169191f82a59
mdzubair21j-sketch <md.zubair21j@gmail.com>
2026-06-27 16:59:00 +0600

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,7 +1,65 @@
 <?php
 
-include '../includes/auth.php';
+require '../includes/database.php';
 
-echo "<h1>Dashboard</h1>";
+$totalCustomers=
+$pdo->query(
+
+"SELECT COUNT(*)
+
+FROM customers"
+
+)->fetchColumn();
+
+
+
+$totalResellers=
+$pdo->query(
+
+"SELECT COUNT(*)
+
+FROM resellers"
+
+)->fetchColumn();
+
+
+
+$totalPackages=
+$pdo->query(
+
+"SELECT COUNT(*)
+
+FROM packages"
+
+)->fetchColumn();
 
 ?>
+
+<h1>
+
+Dashboard
+
+</h1>
+
+<p>
+
+Customers :
+<?=$totalCustomers?>
+
+</p>
+
+
+<p>
+
+Resellers :
+<?=$totalResellers?>
+
+</p>
+
+
+<p>
+
+Packages :
+<?=$totalPackages?>
+
+</p>
